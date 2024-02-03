fec6adac5f5611520bb13361f8a6d407e35154c2
Tangerie <josh.cavill01@gmail.com>
2024-02-03 13:36:15 +1100

    Added ue4ss + refactor

---
+++ docker/app/start.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+die() {
+    echo "$0 script failed, hanging forever..."
+    while [ 1 ]; do sleep 10;done
+    # exit 1
+}
+
+id
+if [ "$(id -u)" != "1000" ];then
+    echo "script must run as steam"
+    die
+fi
+
+steamcmd=${STEAM_HOME}/steamcmd/steamcmd.sh
+
+ACTUAL_PORT=8211
+if [ "${PORT}" != "" ];then
+    ACTUAL_PORT=${PORT}
+fi
+ARGS="${ARGS} -port=${ACTUAL_PORT} -publicport=${ACTUAL_PORT}"
+
+if [ "${PLAYERS}" != "" ];then
+    ARGS="${ARGS} -players=${PLAYERS}"
+fi
+if [ "${SERVER_NAME}" != "" ];then
+    ARGS="${ARGS} -servername=${SERVER_NAME}"
+fi
+if [ "${SERVER_PASSWORD}" != "" ];then
+    ARGS="${ARGS} -serverpassword=${SERVER_PASSWORD}"
+fi
+if [ "${SERVER_PASSWORD}" != "" ];then
+    ARGS="${ARGS} -serverpassword=${SERVER_PASSWORD}"
+fi
+if [ "${NO_MULTITHREADING}" ]; then
+    ARGS=${ARGS}
+else
+    ARGS="${ARGS} -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"
+fi 
+
+# advertise server
+ARGS="${ARGS} EpicApp=PalServer"
+
+PalServerDir=/app/PalServer
+
+mkdir -p ${PalServerDir}
+DirPerm=$(stat -c "%u:%g:%a" ${PalServerDir})
+if [ "${DirPerm}" != "1000:1000:755" ];then
+    echo "${PalServerDir} has unexpected permission ${DirPerm} != 1000:1000:755"
+    die
+fi
+
+set -x
+$steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir ${PalServerDir} +login anonymous +app_update ${APPID} validate +quit || die
+set +x
+
+PalServerExe="${PalServerDir}/Pal/Binaries/Win64/PalServer-Win64-Test.exe"
+if [ ! -f ${PalServerExe} ];then
+    echo "${PalServerExe} does not exist"
+    die
+fi
+
+if [ ! -f "${PalServerDir}/Pal/Binaries/Win64/dwmapi.dll" ]; then
+    echo "Installing UE4SS"
+    cp -r /app/ue4ss/* ${PalServerDir}/Pal/Binaries/Win64/
+fi
+
+crontab /app/crontab || die
+
+CMD="$PROTON run $PalServerExe ${ARGS}"
+echo "starting server with: ${CMD}"
+${CMD} || die
